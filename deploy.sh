56a138325383811bc74c028c03ebdfd71f046f11
danhumassmed <daniel.higgins@umassmed.edu>
2025-04-11 09:55:23 -0400

    updated deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
+# Get current version
+VERSION=$(grep "__version__" pub_worm/__init__.py | cut -d'"' -f2)
+
+IFS='.' read -r -a PARTS <<< "$VERSION"
+MAJOR=${PARTS[0]}
+MINOR=${PARTS[1]}
+PATCH=${PARTS[2]}
+
+# Increment patch
+NEW_VERSION="$MAJOR.$MINOR.$((PATCH + 1))"
+
+echo "Bumping version: $VERSION → $NEW_VERSION"
+
+# Update __init__.py
+sed -i '' "s/__version__ = .*/__version__ = \"$NEW_VERSION\"/" pub_worm/__init__.py
+
 rm -rf ./dist
-rm -rf ./wormcat_batch.egg-info
+rm -rf ./pub_worm.egg-info
+
 python setup.py sdist
-twine check dist/*
-twine upload --repository pypi dist/*
+
+# Run twine check and capture the output
+CHECK_OUTPUT=$(twine check dist/*)
+echo "$CHECK_OUTPUT"
+
+if echo "$CHECK_OUTPUT" | grep -q "PASSED"; then
+    echo "Twine check passed, uploading to PyPI..."
+    twine upload --repository pypi dist/*
+
+    # Git commit, tag, and push
+    git add "$VERSION_FILE"
+    git commit -m "Bump version to $NEW_VERSION"
+    git tag "v$NEW_VERSION"
+    git push
+    git push --tags
+else
+    echo "Twine check failed..."
+    exit 1
+fi
--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 """
 Setup for pypi releases of pub_worm
 """
+from pub_worm import __version__
 from setuptools import setup, find_packages
 from pathlib import Path
-
-# rm -rf dist
-# python setup.py sdist
-# pip install dist/wormcat_batch-1.0.1.tar.gz
-# twine check dist/*
-# twine upload --repository pypi dist/*
 
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='pub_worm',
-      version='0.3.7',
+      version=__version__,
       description='Wormbase/PudMed API Access',
       long_description_content_type="text/markdown",
       long_description=long_description,
@@ -26,6 +21,12 @@ setup(name='pub_worm',
       license='MIT',
 
       packages=find_packages(),
-      install_requires=['pandas'],
+      install_requires=[
+            'pandas==2.2.3',
+            'beautifulsoup4==4.12.3',
+            'requests==2.31.0',
+            'aiohttp==3.9.5',
+            'aiofiles==24.1.0'
+      ],
       include_package_data=True,
       zip_safe=False)
