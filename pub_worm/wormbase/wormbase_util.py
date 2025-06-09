21630c2855a2d6bf57613a670e618b31d6ac101b
danhumassmed <daniel.higgins@umassmed.edu>
2025-06-09 12:00:22 -0400

    add map wormbase ids

--- pub_worm/wormbase/wormbase_util.py
+++ pub_worm/wormbase/wormbase_util.py
@@ -3,9 +3,12 @@ import requests
 import gzip
 import shutil
 import csv
+import sys
 import json
+import re
 from bs4 import BeautifulSoup
 import pandas as pd
+from pathlib import Path
 
 # Get the most current Wormbase DB
 def current_wormbase_version():
@@ -93,12 +96,13 @@ def download_annotation_file(wormbase_version, file_nm, output_dir):
             os.remove(output_file_path)
             
         print(f"Unzipped: {output_file_path}")
+        return output_file_path[:-3]
     
 def download_gene_ids(wormbase_version, output_dir):
         return download_annotation_file(wormbase_version, "geneIDs.txt.gz", output_dir)
     
         
-def extract_live_gene_ids(wormbase_version, source_dir):
+def gene_ids_to_csv(wormbase_version, source_dir, status_live=True):
     gene_ids = f"c_elegans.PRJNA13758.{wormbase_version}.geneIDs.txt"
     input_file = f"{source_dir}/{gene_ids}"
 
@@ -110,19 +114,94 @@ def extract_live_gene_ids(wormbase_version, source_dir):
     output_file = f"{input_file[:-3]}csv"
 
     # Load the input CSV file into a DataFrame
-    df = pd.read_csv(input_file, header=None)
-
-    # Filter rows where the 5th column equals 'Live'
-    df_filtered = df[df[4] == 'Live']
-
-    # Select the required columns (2nd, 3rd, 4th, and 6th)
-    df_selected = df_filtered[[1, 2, 3, 5]]
-
-    # Add the appropriate column headers
-    df_selected.columns = ["Wormbase_Id", "Gene_name", "Sequence_id", "Gene_Type"]
-
+    gene_ids_df = pd.read_csv(input_file, header=None)
+    if status_live:
+        # Filter rows where the 5th column equals 'Live'
+        gene_ids_df = gene_ids_df[gene_ids_df[4] == 'Live']
+        gene_ids_df = gene_ids_df[[1, 2, 3, 5]]
+        gene_ids_df.columns = ["Wormbase_Id", "Gene_name", "Sequence_id", "Gene_Type"]
+    else:
+        gene_ids_df = gene_ids_df[[1, 2, 3, 4, 5]]
+        gene_ids_df.columns = ["Wormbase_Id", "Gene_name", "Sequence_id", "Status", "Gene_Type"]
+        
     # Save the result to the output file
-    df_selected.to_csv(output_file, index=False)
+    gene_ids_df.to_csv(output_file, index=False)
 
     print(f"Processed file saved to: {output_file}")
- 
+    return output_file
+
+
+
+def _clean_sequence_id(sequence_id):
+    if '.' in sequence_id:
+        base, isoform = sequence_id.rsplit('.', 1)
+        if re.fullmatch(r'\d+[A-Z]', isoform):
+            return f"{base}.{isoform[:-1]}"
+    return None
+
+def map_wormbase_ids(sequence_ids_file_path, working_dir_path=None):
+    if not working_dir_path:
+        working_dir_path = Path(sequence_ids_file_path).parent
+    
+    sequence_ids_df = pd.read_csv(sequence_ids_file_path)
+    if 'ID' not in sequence_ids_df.columns:
+        print("ID column is required in the input CSV")
+        sys.exit(1)
+    
+    wormbase_version = current_wormbase_version()
+    gene_ids_txt = download_gene_ids(wormbase_version, working_dir_path)
+    
+    gene_ids_csv = gene_ids_to_csv(wormbase_version, working_dir_path, status_live=False)
+    gene_ids_df = pd.read_csv(gene_ids_csv)
+    print(f"Created {wormbase_version} version of wormbase csv")
+
+    # Remove the .txt
+    if os.path.exists(gene_ids_txt):
+        os.remove(gene_ids_txt)
+            
+    gene_ids_dict = {}
+    for _, row in gene_ids_df.iterrows():
+        for key in ['Wormbase_Id', 'Gene_name', 'Sequence_id']:
+            id_val = str(row[key]).upper()
+            gene_ids_dict[id_val] = row.to_dict()
+            
+    found_ids = []
+    not_found_ids = []
+
+    for _, row in sequence_ids_df.iterrows():
+        sequence_id = str(row['ID']).upper()
+        found = gene_ids_dict.get(sequence_id)
+        if not found and sequence_id.startswith("PSEUDOGENE:"):
+            sequence_id = sequence_id[11:]
+            found = gene_ids_dict.get(sequence_id)
+        if not found and _clean_sequence_id(sequence_id):
+            sequence_id = _clean_sequence_id(sequence_id)
+            found = gene_ids_dict.get(sequence_id)
+        if not found and sequence_id.rindex('.') > -1:
+            sequence_id = sequence_id[0:sequence_id.rindex('.')]
+            found = gene_ids_dict.get(sequence_id)
+        if not found and _clean_sequence_id(sequence_id):
+            sequence_id = _clean_sequence_id(sequence_id)
+            found = gene_ids_dict.get(sequence_id)
+        if found:
+            found['Initial_Alias']=row['ID']
+            found_ids.append(found)
+        else:
+            not_found_ids.append(row['ID'])
+            
+    num_found = len(found_ids)
+    num_not_found = len(not_found_ids)
+    totals = num_found + num_not_found
+    percent_found = num_found / totals *100
+    
+    print(f"Found     {num_found:>6,} genes.")
+    print(f"Not Found {num_not_found:>6,} genes.")
+    print(f"Processed {totals:>6,} genes.  {percent_found:.2f}% matched.")
+
+    not_found_ids= sorted(not_found_ids)
+    df = pd.DataFrame(not_found_ids, columns=['ID'])
+    df.to_csv(working_dir_path / 'wormbase_ids_not_found.csv', index=False)
+    
+    found_ids = sorted(found_ids, key=lambda d: d['Wormbase_Id'])
+    df = pd.DataFrame(found_ids)
+    df.to_csv(working_dir_path / 'wormbase_ids_found.csv', index=False)
